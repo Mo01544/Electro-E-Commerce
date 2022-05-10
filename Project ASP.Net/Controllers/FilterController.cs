b21b9e9a8893bd74b39b4ff4310d4d4962932d50
Amr M. Nouh <AMR.MAMDOUH1371@ci.menofia.edu.eg>
2022-05-10 10:40:25 +0200

    start creating Filter Feature

---
+++ Project ASP.Net/Controllers/FilterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_ASP.Net.Controllers
+{
+    public class FilterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
